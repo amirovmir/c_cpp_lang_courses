0bae1c47355efbb24b94aab757c1cc6e7eccdfe1
amirovmir <78919537+amirovmir@users.noreply.github.com>
2021-11-04 12:55:24 +0300

    Create Task_6-1.cpp

---
+++ Lecture/LK-6/Task_6-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;	int i = -1;
+	cout << "Enter a number of numbers" << endl;
+	cin >> n;
+	vector<int> num(n + 1);
+
+	generate(num.begin(), num.end(), [&i] {i++; return i; });
+	copy(num.begin(), num.end(), ostream_iterator<int>(cout, " "));
+    return 0;
+}
