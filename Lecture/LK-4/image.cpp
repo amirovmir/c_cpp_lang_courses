3b3d9674c3731c0f678285ac94aff7dce072176f
amirovmir <78919537+amirovmir@users.noreply.github.com>
2021-11-17 22:05:47 +0300

    Update image.cpp

--- Lecture/LK-4/image.cpp
+++ Lecture/LK-4/image.cpp
@@ -8,64 +8,61 @@
 using namespace std;
 
 class Image {
-    int* img;
-    int mx, my;
-public:
-    Image(int tmx, int tmy) : mx(tmx), my(tmy) {
-        img = new int[mx * my]();
-    }
-    virtual ~Image() {
-        delete[] img;
-    };
-    int get(int x, int y) {
-        try {
-            if (x > mx || y > my || x < 0 || y < 0)
-                throw 404;
-        }
-        catch (int error) {
-            cout << "Eror " << error << endl;
-            exit(1);
-        }
+	int* img;
+	int mx, my;
 
-        return img[y * my + x];
-    }
-    
-    int set(int x, int y, int color) {
-        try {
-            if (x >= mx || y >= my || x < 0 || y < 0) 
-                throw 404;
-        }
-        catch (int error) {
-            cout << "Eror " << error << endl;
-            exit(1);
-        }
-        img[y * my + x] = color;
-    }
-    Image operator=(const Image& v) {
-        mx = v.mx; 
-        my = v.my;
-        return *this;
-    }
+public:
+	Image(int tmx, int tmy) : mx(tmx), my(tmy) {
+		img = new int[mx * my]();
+	};
+
+	virtual ~Image() {
+		delete[] img;
+	};
+
+	int get(int x, int y);
+	void set(int x, int y, int color);
+
+	Image operator=(const Image& v) {
+		this->mx = v.mx;
+		this->my = v.my;
+		return *this;
+	};
+	void show();
 };
 
+int Image::get(int x, int y) {
+	if (x >= 0 && x < mx && y >= 0 && y < my)
+		return img[y * my + x];
+}
+
+void Image::set(int x, int y, int color) {
+	if (x >= 0 && x < mx && y >= 0 && y < my)
+		img[y * my + x] = color;
+}
+
+void Image::show() {
+	for (int i = 0; i < my; ++i) {
+		for (int j = 0; j < mx; ++j) {
+			cout << img[i * my + j] << ' ';
+		}
+		cout << endl;
+	}
+}
+
 int main() {
-    try {
-        Image img1(5, 5);
-        img1.set(4, 0, 1);
-        img1.set(1, 2, 1);
+	Image img1(4, 5);
+	img1.set(4, 0, 1);
+	img1.set(1, 2, 1);
 
-        Image img2(5, 5);
-        img2.set(1, 3, 2);
-        img2.set(1, 0, 1);
+	Image img2 (3, 4);
+	img2.set(1, 3, 2);
+	img2.set(1, 0, 1);
 
-        if (img1 == img2) {
-            cout << "Error, same format" << endl;
-        }
+	cout << img1.get(1, 2) << endl;
+	cout << img2.get(1, 3) << endl;
 
-        cout << img1.get(5, 1) << endl;
-        cout << img2.get(2, 4) << endl;
-    }
-    catch (int) {
-        cout << "Out of range\n";
-    }
+	img1.show();
+
+	return 0;
 }
